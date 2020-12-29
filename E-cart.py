13b7db9690a4f107b00a3ba7d237ae7f8f3f4a50
Nim's Luhar <pnimesh555557@gmail.com>
2020-12-29 16:49:45 +0530

    werkzeug e-cart

--- E-cart.py
+++ E-cart.py
@@ -89,6 +89,7 @@ def Ecart(request):
         </body>
     ''')
     return Response(''.join(result), mimetype='text/html')
+
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple('localhost', 5000, Ecart)
