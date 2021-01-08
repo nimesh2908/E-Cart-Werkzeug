db11e54a6213df6fe01ab2d7ccc6eb0e11eef608
Nim's Luhar <pnimesh555557@gmail.com>
2021-01-08 15:35:34 +0530

    Message

--- E-cart.py
+++ E-cart.py
@@ -2,18 +2,68 @@
 # -*- coding: utf-8 -*-
 
 import json
-#from werkzeug.utils import escape
+from tempfile import gettempdir
+from werkzeug.contrib.sessions import FilesystemSessionStore
+from werkzeug.routing import Map, Rule
 from werkzeug.wrappers import Request, Response
+
+def index(request):
+    html = open('Demo.html', 'r').read()
+    data = open('Cart.json', 'r').read()
+
+    cartData = 'false'
+
+    if bool(int(request.session.__len__())):
+        cartData = request.session['product_ids']
+    response = Response(html % {'data': str(json.loads(data)),
+                                 'cartData': cartData})
+    response.status = '200 OK'
+    response.headers['content-type'] = 'text/html'
+    return response
+
+def cart(request, product_id=None):
+    
+    if product_id and not bool(int(request.session.__len__())):
+        request.session['product_ids'] = {product_id: product_id}
+    else:
+        request.session['product_ids'].update({product_id: product_id})
+    return index(request)
+
+
+def remove(request, product_id=None):
+    if product_id and bool(int(request.session['product_ids'].__len__())):
+        request.session['product_ids'].pop(product_id)
+    return index(request)
+
+
+url_map = Map([
+    Rule("/", endpoint="index"),
+    Rule("/add/to/cart/<int:product_id>", endpoint="cart"),
+    Rule("/remove/<int:product_id>", endpoint="remove")
+])
+
+views = {
+    "index": index,
+    "cart": cart,
+    "remove": remove
+}
+
+session_store = FilesystemSessionStore(path=gettempdir())
 
 @Request.application
 def Ecart(request):
-    html = open('Demo.html', 'r').read()
-    data = open('Cart.json', 'r').read()
-    response = Response(html % {'data': str(json.loads(data))})
-    response.status = '200 OK'
-    response.headers['content-type'] = 'text/html'
+    sid = request.cookies.get("session_id")
+    if sid is None:
+        request.session = session_store.new()
+    else:
+        request.session = session_store.get(sid)
+    url_adapter = url_map.bind_to_environ(request.environ)
+    endpoint, values = url_adapter.match()
+    response = views[endpoint](request, **values)
+    session_store.save(request.session)
+    response.set_cookie("session_id", request.session.sid)
     return response
     
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('localhost', 16000, Ecart)
+    run_simple('localhost', 5000, Ecart)
