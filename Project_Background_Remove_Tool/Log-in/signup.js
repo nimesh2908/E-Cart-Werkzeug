95fb22bc0bd02d1b59942cba156bf8896ec573a2
Nim's Luhar <pnimesh555557@gmail.com>
2021-02-03 18:56:43 +0530

    Done

--- Project_Background_Remove_Tool/Log-in/signup.js
+++ Project_Background_Remove_Tool/Log-in/signup.js
@@ -1,17 +1,28 @@
 const { Component, Store, mount } = owl;
 const { xml } = owl.tags;
 
-export class signup extends Component {
-	static template = xml`	<div>
-								<label>Email:</label>
-								<input type="text" placeholder="Enter Your Email Address"/>
-								<label>Mobile Number</label>
-								<input type="text" placeholder="Enter Mobile Number"/>
-								<label>New Password</label>
-								<input type="Password" name="pass"/>
-								<label>Re-enter Password</label>
-								<input type="Password" name="rpass"/>
-								<input type="button" name="btn" value="submit"/>
+export class Signup extends Component {
+	static template = xml`	<div id="dv">
+									<tr id="tr">	
+									<label>Email:</label>
+									<input type="text" placeholder="Enter Your Email Address"/>
+									</tr>
+									<tr>
+									<label>Mobile Number</label>
+									<input type="text" placeholder="Enter Mobile Number"/>
+									</tr>
+									<tr>
+									<label>New Password</label>
+									<input type="Password" name="pass"/>
+									</tr>
+									<tr>
+									<label>Re-enter Password</label>
+									<input type="Password" name="rpass"/>
+									</tr>
+									<tr>
+									<input type="button" name="btn" value="submit"/>
+									</tr>
 							</div>`;
+
 
 }
--- Project_Background_Remove_Tool/Log-in/validate.js
+++ Project_Background_Remove_Tool/Log-in/validate.js
@@ -2,41 +2,46 @@ const { Component, Store, mount } = owl;
 const { xml } = owl.tags;
 const { whenReady } = owl.utils;
 const { useRef, useDispatch, useState, useStore } = owl.hooks;
+const {qweb} = owl;
 
-import {signup} from "./signup.js"
-
-export const ROUTES = [
-  { name: "SIGNUP", path: "/signup" },
-];
+import { Signup } from "./signup.js";
 
 const APP_TEMPLATE = xml/* xml */ `
 <div>
 	<h1 align="center">Background Remove Tool</h1>
 		<div align="center">
 			<input type="button" value="Sign-Up" t-on-click="signup"/>
+		<Signup/>
 		</div>
 </div>`;
 
-class brt extends Component {
+class BRT extends Component {
 	static template =APP_TEMPLATE;
+	static components={Signup};
 
-	signup(env,to){
-		return { to: "SIGNUP" };
-		const sgout=new signup();
+	signup(){
+		alert("Welcome");
+		/*return this.env.router.navigate({ to: 'Signup'});*/
 		alert("logout");
 	}
-}
 
-function makeEnvironment() {
+	
+}
+/*export const ROUTES = [
+	  { name: "SIGNUP", path: "./signup", component: Signup },
+];*/
+
+/*function makeEnvironment() {
     const env = { qweb };
-    env.session = new Session(env);
-    env.router = new owl.router.Router(env, ROUTES);
+    const router = new owl.router.Router(env, ROUTES);
     env.router.start();
     return env;
 }
+	BRT.env=makeEnvironment();*/
+
 // Setup code
 function setup() {
-	const bgrmtl=new brt();
+	const bgrmtl=new BRT();
  	bgrmtl.mount(document.body);
 }
 
